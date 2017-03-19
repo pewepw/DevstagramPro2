2aff6f306aa33e7ed26178d4f7a50f7d394a210d
Harry Ng <poripori1030@gmail.com>
2017-03-19 18:38:19 +0800

    Send Comments- Design Scalable Comment Databases

--- DevstagramPro2/CommentViewController.swift
+++ DevstagramPro2/CommentViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+import SVProgressHUD
 
 class CommentViewController: UIViewController {
     
 
+    @IBOutlet weak var commentTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sendButton.setTitleColor(UIColor.lightGray, for: .normal)
+        sendButton.isEnabled = false
+        
+        handleTextField()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,5 +33,55 @@ class CommentViewController: UIViewController {
         
     }
     
+    @IBAction func sendButton(_ sender: Any) {
+        
+        let ref = FIRDatabase.database().reference()
+        let commentsReference = ref.child("comments")
+        let newCommentId = commentsReference.childByAutoId().key
+        let newCommentsReference = commentsReference.child(newCommentId)
+        
+        guard let currentUser = FIRAuth.auth()?.currentUser else {
+            return
+        }
+        
+        let currentUserId = currentUser.uid
+        newCommentsReference.setValue(["uid": currentUserId, "commentText": commentTextField.text!]) { (error, ref) in
+            if error != nil {
+                
+                SVProgressHUD.showError(withStatus: error!.localizedDescription)
+                return
+                
+            }
+            
+            self.empty()
+           
+        }
+        
+    }
+    
+    func empty() {
+        self.commentTextField.text = ""
+        self.sendButton.isEnabled = false
+        self.sendButton.setTitleColor(UIColor.lightGray, for: .normal)
+    }
+    
+    func handleTextField() {
+        commentTextField.addTarget(self, action: #selector(self.textFieldDidChange), for: .editingChanged)
+    }
+
+    
+    func textFieldDidChange() {
+        if let commentText = commentTextField.text, !commentText.isEmpty {
+            sendButton.setTitleColor(UIColor.black , for: .normal)
+            sendButton.isEnabled = true
+            
+            return
+        }
+        
+        
+        sendButton.setTitleColor(UIColor.lightGray , for: .normal)
+        sendButton.isEnabled = false
+    }
+
     
 }
