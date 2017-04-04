020eb639175a8a56695b68c57c936d38cef191a2
Harry Ng <poripori1030@gmail.com>
2017-04-05 01:33:09 +0800

    no message

---
+++ DevstagramPro2/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  DevstagramPro2
+//
+//  Created by Harry Ng on 04/04/2017.
+//  Copyright © 2017 DevConcept. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var postId = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+  
+
+}
